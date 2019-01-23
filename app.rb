c44a6d2b223de727a9886bca65bd54c8eaef8e87
Joey Cho <cjy12182000@naver.com>
2019-01-23 07:26:49 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -12,6 +12,12 @@ class App < Sinatra::Base
   
   post '/team' do
     @teamname = params[:teamname]
+    @coach = params[:coach]
+    @pointguard = params[:pointguard]
+    @shootingguard = params[:shootingguard]
+    @sf =params[:sf]
+    @pf= params[:pf]
+    @c = params[:c]
     erb :team
   end
 
